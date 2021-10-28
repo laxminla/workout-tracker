e4c43a7ce3d583b0e4a0d07705d803064edada14
Laxmi Sharma <laxminla@yahoo.com>
2021-10-27 19:40:27 -0700

    modified workout.js

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -2,22 +2,57 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-    name: {
-      type: String,
-      trim: true,
-      required: "Enter a name for transaction"
-    },
-    value: {
-      type: Number,
-      required: "Enter an amount"
-    },
-    date: {
+const WorkoutSchema = new Schema(
+  {
+    day: {
       type: Date,
       default: Date.now
-    }
+  },
+  exercises: [
+      {
+          type: {
+              type: String
+          },
+          name: {
+              type: String,
+              trim: true,
+          },
+          duration: {
+              type: Number
+          },
+          distance: {
+              type: Number
+          },
+          weight: {
+              type: Number
+          },
+          reps: {
+              type: Number
+          },
+          sets: {
+              type: Number
+          }
+      }
+  ]
+},
+
+{
+  toJSON: {
+      virtuals: true
+  }
+  }
+);
+
+WorkoutSchema.virtual("totalDuration").get(function() {
+  var sumTotal = 0;
+  this.exercises.forEach(object => {
+    sumTotal += object.duration
   });
+  return sumTotal;
+});
   
-  const Transaction = mongoose.model("Transaction", transactionSchema);
+
+// exporting the WorkoutSchema as a model
+  const Workout = mongoose.model("Workout", WorkoutSchema);
   
-  module.exports = Transaction;
+  module.exports = Workout;
