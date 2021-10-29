ee8e1ae7d5d79f0e1168beaaec5c9a0aa04f090a
Laxmi Sharma <laxminla@yahoo.com>
2021-10-28 17:07:20 -0700

    created codes in html-routes.js

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,6 +1,53 @@
 const db = require("../models");
 const router = require("express").Router()
 
+// GET route to get the last workout 
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout)
+    })
+    .catch(err => {
+        res.json(err);
+      });
+});
+
+// PUT route to update an exercise in the workout
+router.put("/api/workouts/:id", (req, res) => {
+console.log(req.body);
+console.log(req.params.id);
+
+db.Workout.findByIdAndUpdate (
+    req.params.id,
+    {
+        $push: {
+            exercises: req.body
+        }
+    },
+    (error, dbWorkout) => {
+        if (error) {
+            res.send(error)
+        } else {
+            res.send(dbWorkout);
+        }
+    }
+);
+});
+
+// POST route to create a workout
+router.post("/api/workouts", (req, res) => {
+    db.Workout.create({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+
+
+// GET route to get workout in a range
 
 
 
--- Develop/routes/html-routes.js
+++ Develop/routes/html-routes.js
@@ -1,6 +1,22 @@
+// required dependency
 const path = require("path");
+// epmorting router method
 const router = require("express").Router();
 
+// get route for index.html page
 router.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+
+// GET route for exercise html page
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+// GET route for stats.html page
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+// exporting routes to the server
+module.exports = router
